c291a584dd43d80c5a1da98655953abd639c0e9c
vrramosm <vf.ramos7@gmail.com>
2020-08-19 15:10:19 -0300

    Feito algumas partes do requisito 1

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -1,2 +1,35 @@
-def scrape():
-    raise NotImplementedError
+from parsel import Selector
+import requests
+
+
+def fetch_content(url, timeout=1):
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return ""
+    else:
+        return response.text
+
+
+def scrape(page=1):
+    base_url = f"https://www.tecmundo.com.br/novidades?page={page}"
+    news = []
+    content = fetch_content(base_url)
+    selector = Selector(content)
+    for new in selector.css("div.tec--list__item"):
+        url = new.css("a.tec--card__title__link::attr(href)").get()
+        title = new.css("a.tec--card__title__link::text").get()
+        timestamp = new.css(
+            "div.tec--timestamp__item.z--font-semibold::text"
+        ).get()
+        
+        news.append({
+            "url": url,
+            "title": title,
+            "timestamp": timestamp
+        })
+    return news
+
+
+print(scrape(2))
