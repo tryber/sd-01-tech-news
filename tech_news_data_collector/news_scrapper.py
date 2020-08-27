67cdfc2b09a961fc02281ae4c0c7b675ec212b25
antoniopaolillo <antonio_francisco_p@live.com>
2020-08-27 15:55:16 -0300

    create scrape and start project

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -1,2 +1,21 @@
-def scrape():
-    raise NotImplementedError
+from parsel import Selector
+import requests
+
+
+URL_BASE = "https://www.tecmundo.com.br/novidades"
+next_page_url = '?page=1'
+
+
+def scrape(number_of_pages=1):
+    while number_of_pages > 0:
+        response = requests.get(URL_BASE + next_page_url)
+        selector = Selector(text=response.text)
+
+        for notice in selector.css(".tec--list__item"):
+            title = notice.css(".tec--card__title__link::text").get()
+            
+            print(title)
+        number_of_pages -= 1
+
+
+scrape()
