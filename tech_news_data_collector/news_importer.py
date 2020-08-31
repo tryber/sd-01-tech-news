393608cd041a7dca91a2800363ba4e97fe5a4a2b
antoniopaolillo <antonio_francisco_p@live.com>
2020-08-31 16:06:53 -0300

    add function csv_importer

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,5 +1,45 @@
-def csv_importer():
-    raise NotImplementedError
+import csv
+from pymongo import MongoClient
+
+necessary_headers = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories"
+]
+
+
+def csv_importer(path_to_file):
+    if not path_to_file:
+        print(f"Arquivo {path_to_file} não encontrado")
+
+    with open(path_to_file) as file:
+        file_status = csv.reader(file, delimiter=",")
+        header, *data = file_status
+
+    intersection = set(header) & set(necessary_headers)
+
+    if(len(intersection) != 9):
+        print("Cabeçalho inválido")
+
+    for row in data:
+        if(len(row) != 9):
+            print(f"Erro na notícia {row[0]}")
+
+    client = MongoClient()
+    db = client.tech_news
+    try:
+        db.notices.updateMany(data, upsert=True)
+        client.close()
+    except client:
+        print(client.errors)
+
+    print("Importação realizada com sucesso")
 
 
 def json_importer():
--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -3,19 +3,72 @@ import requests
 
 
 URL_BASE = "https://www.tecmundo.com.br/novidades"
-next_page_url = '?page=1'
 
 
 def scrape(number_of_pages=1):
-    while number_of_pages > 0:
-        response = requests.get(URL_BASE + next_page_url)
+    page_number = 1
+    next_url_page = "?page=1"
+    while page_number <= number_of_pages:
+        response = requests.get(URL_BASE + next_url_page)
         selector = Selector(text=response.text)
 
         for notice in selector.css(".tec--list__item"):
             title = notice.css(".tec--card__title__link::text").get()
-            
-            print(title)
-        number_of_pages -= 1
+            url = notice.css(".tec--card__title__link::attr(href)").get()
+
+            # Baixa o conteúdo da página de detalhes
+            detail_response = requests.get(url)
+            detail_selector = Selector(text=detail_response.text)
+
+            timestamp = detail_selector.css("#js-article-date::text").get()
+            writer = detail_selector.css(
+                ".tec--author__info__link::text"
+                ).get()
+            shares_count = detail_selector.css(
+                ".tec--toolbar__item::text"
+                ).get()
+            comments_count = detail_selector.css(
+                "#js-comments-btn::text"
+                ).get().split(' ', 1)
+            summary = detail_selector.css(
+                ".tec--article__body z--px-16 p402_premium *::text"
+                ).get()
+            sources = detail_selector.css(".tec--badge::text").getall()
+            categories = detail_selector.css(
+                ".tec--badge .tec--badge--primary::text"
+                ).get()
+            print(
+                    title,
+                    'titulo',
+                    '\n',
+                    url,
+                    'url',
+                    '\n',
+                    timestamp,
+                                     'timestamp',
+                    '\n',
+                    writer,
+                                     'writer',
+                    '\n',
+                    shares_count,
+                                     'shares_count',
+                    '\n',
+                    comments_count,
+                                     'comments_count',
+                    '\n',
+                    summary,
+                                     'summary',
+                    '\n',
+                    sources,
+                                     'sources',
+                    '\n',
+                    categories,
+                                     'categories',
+                    '\n',
+                )
+
+        page_number += 1
+        next_url_page = f"?page={page_number}"
 
 
 scrape()
