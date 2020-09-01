513836f75c1a96e2bbe7f910370bc572a3305a62
vrramosm <vf.ramos7@gmail.com>
2020-09-01 14:39:39 -0300

    Iniciando uma nova forma de fazer o requisito 1

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -12,23 +12,30 @@ def fetch_content(url, timeout=1):
         return response.text
 
 
-def scrape(page=1):
-    base_url = f"https://www.tecmundo.com.br/novidades?page={page}"
+def get_all_attributes(url):
     news = []
-    content = fetch_content(base_url)
+    title = ''
+    content = fetch_content(url)
     selector = Selector(content)
-    for new in selector.css("div.tec--list__item"):
-        url = new.css("a.tec--card__title__link::attr(href)").get()
-        title = new.css("a.tec--card__title__link::text").get()
-        timestamp = new.css(
-            "div.tec--timestamp__item.z--font-semibold::text"
-        ).get()
-        news.append({
-            "url": url,
-            "title": title,
-            "timestamp": timestamp
+    for new in selector.css("div.z--pt-40.z--pb-24.z--pl-16"):
+        title = new.css("h1.tec--article__header__title::text").get()
+        print(title)
+        return news.append({
+            "title": title
         })
     return news
 
 
-print(scrape(2))
+def scrape(page=1):
+    all_news = []
+    base_url = f"https://www.tecmundo.com.br/novidades?page={page}"
+    content = fetch_content(base_url)
+    selector = Selector(content)
+    for new in selector.css("div.tec--list__item"):
+        url = new.css("a.tec--card__title__link::attr(href)").get()
+        news = get_all_attributes(url)
+        all_news.append(news)
+    return all_news
+
+
+print(scrape())
