a8732fa0b12104a65b65a543e503576d94c86b8d
Guilherme Luiz <guiiluiz44@gmail.com>
2020-09-02 18:27:10 -0300

    get data from database to create json and csv files

--- tech_news_data_collector/news_exporter.py
+++ tech_news_data_collector/news_exporter.py
@@ -1,10 +1,22 @@
 import json
 import csv
 from news_scrapper import scrape
+from pymongo import MongoClient
+
+
+def get_data_from_db():
+    data = []
+    db_client = MongoClient()
+    db = db_client.tech_news
+    col = db["news"]
+    news_data = col.find(projection={'_id': False})
+    for news in news_data:
+        data.append(news)
+    return data
 
 
 def csv_exporter():
-    news_data = scrape()
+    news_data = get_data_from_db() or scrape()
     with open('tech_news.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(["url", "title", "timestamp", "writer", "shares_count", "comments_count", "summary", "sources", "categories"])
@@ -14,10 +26,6 @@ def csv_exporter():
 
 
 def json_exporter():
-    news_data = scrape()
+    news_data = get_data_from_db() or scrape()
     f = open('tech_news.json', 'w')
     f.write(json.dumps(news_data, indent=2))
-
-
-json_exporter()
-csv_exporter()
