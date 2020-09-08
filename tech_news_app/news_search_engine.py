bbcbb4cd0b1b328d604d13643c9426fde7750b45
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-07 23:03:04 -0300

    filtrando itens por data, requisito 7

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -32,3 +32,28 @@ def search_by_title(title):
         )
     print("title", received_data)
     return received_data
+
+
+def validate_date(date):
+    if len(date) == 0:
+        return []
+    splited_date = date.split("/")
+    return (
+        len(splited_date[0]) == 4
+        and len(splited_date[1]) == 2
+        and len(splited_date[2]) == 2
+    )
+
+
+def format_date(date):
+    replace_date = date.replace("/", "-")
+    return replace_date
+
+
+def search_by_date(date):
+    if validate_date(date) is False:
+        return print("Data inválida")
+    formated_date = format_date(date)
+    received_data = get_data_from_database({"timestamp": formated_date})
+    print("date", received_data)
+    return received_data
