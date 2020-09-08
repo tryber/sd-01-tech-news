1eb20d5a08beda9ace16fc1bb45c1f304a9b4ca3
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-07 23:50:37 -0300

    finalizando exportação de arquivo do mongodb para a aplicação

--- tech_news_data_collector/news_exporter.py
+++ tech_news_data_collector/news_exporter.py
@@ -1,6 +1,38 @@
-def csv_exporter():
-    raise NotImplementedError
+from pymongo import MongoClient
+import json
+import sys
+
+# Código baseado no projeto do Conrado
+# https://github.com/tryber/sd-01-tech-news/tree/conradomedeiros-sd-01-tech-news
 
 
-def json_exporter():
-    raise NotImplementedError
+def validate_path_file(path, format):
+    array_of_path = path.split(".")
+    if array_of_path[1] == format:
+        return True
+    return False
+
+
+def get_data_from_database():
+    client = MongoClient()
+    db = client.tech_news
+    array_of_notices = []
+    try:
+        notices = db.news.find({}, {"_id": 0})
+        for notice in notices:
+            array_of_notices.append(notice)
+        client.close()
+        return array_of_notices
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+
+def json_exporter(path_to_file="tech_news.json"):
+    if not validate_path_file(path_to_file, "json"):
+        return print("Formato inválido")
+
+    notices = get_data_from_database()
+
+    with open(f"{path_to_file}", "w") as file:
+        json.dump(notices, file)
+    print("Exportação realizada com sucesso")
