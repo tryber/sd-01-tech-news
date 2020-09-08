4b0deeeed3bc53a1e90d77bee64cedf93ecec3d3
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-07 23:01:47 -0300

    finalizando estrutura do requisito 6

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,14 +1,34 @@
-def search_by_title():
-    raise NotImplementedError
+from pymongo import MongoClient
+
+# Todo o código abaixo foi feito com a ajuda do conrado
+# https://github.com/tryber/sd-01-tech-news/tree/conradomedeiros-sd-01-tech-news
 
 
-def search_by_date():
-    raise NotImplementedError
+def get_data_from_database(search):
+    client = MongoClient()
+    db = client.tech_news
+    array_of_notices = []
+    try:
+        notices = db.news.find(search, {"_id": 0, "title": 1, "url": 1})
+        print("notices", notices)
+        for notice in notices:
+            array_of_notices.append(notice)
+        client.close()
+        return array_of_notices
+    except client:
+        return print(client.errors)
 
 
-def search_by_source():
-    raise NotImplementedError
+def validate_title(title):
+    if len(title) == 0:
+        return []
 
 
-def search_by_category():
-    raise NotImplementedError
+def search_by_title(title):
+    if validate_title(title) == []:
+        return print("dados invalidos")
+    received_data = get_data_from_database(
+        {"title": {"$regex": title, "$options": "i"}}
+        )
+    print("title", received_data)
+    return received_data
