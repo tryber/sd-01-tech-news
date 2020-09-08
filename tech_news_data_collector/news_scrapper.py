64f050491def3268c342eaccae5fdcff94592f6d
vrramosm <vf.ramos7@gmail.com>
2020-09-08 10:43:08 -0300

    Refazendo o requisito 1

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -1,41 +1,139 @@
 from parsel import Selector
 import requests
+import time
+from pymongo import MongoClient
+
+# Código criado com ajuda do Gui e do Conrado!
+# https://github.com/tryber/sd-01-tech-news/blob/guiiluiz-tech-news/
+# https://github.com/tryber/sd-01-tech-news/tree/conradomedeiros-sd-01-tech-news
 
 
-def fetch_content(url, timeout=1):
-    try:
-        response = requests.get(url, timeout=timeout)
-        response.raise_for_status()
-    except (requests.HTTPError, requests.ReadTimeout):
-        return ""
+def get_news_title(content):
+    selector = Selector(content)
+    title = selector.css(".tec--article__header__title::text").get()
+    return title.strip()
+
+
+def get_news_timestamp(content):
+    selector = Selector(content)
+    return selector.css(".tec--timestamp__item").xpath('./time/@datetime').get()
+
+
+def get_news_writer(content):
+    selector = Selector(content)
+    author = selector.css(".tec--author__info__link::text").get()
+    if author is None:
+        writer = selector.xpath("//div[contains(@class, 'tec--timestamp__item') and contains(@class, 'z--font-bold')]").xpath(".//a/text()").get()
+        if writer:
+            return writer.strip()
+        else:
+            name = selector.xpath("//p[contains(@class, 'z--m-none') and =contains(@class, 'z--truncate') and contains(@class,
+            'z--font-bold')]/text()").get()
+            if name:
+                return name.strip()
+    return author.strip()
+
+
+def get_news_shares(content):
+    selector = Selector(content)
+    shares = selector.css(".tec--toolbar__item::text").getall()
+    if shares:
+        return shares[1].strip().split(' ')[0]
     else:
-        return response.text
+        return 0
 
 
-def get_all_attributes(url):
-    news = []
-    title = ''
-    content = fetch_content(url)
+def get_news_comments(content):
     selector = Selector(content)
-    for new in selector.css("div.z--pt-40.z--pb-24.z--pl-16"):
-        title = new.css("h1.tec--article__header__title::text").get()
-        print(title)
-        return news.append({
-            "title": title
-        })
-    return news
+    return selector.xpath("//button[@id='js-comments-btn']/@data-count").get()
 
 
-def scrape(page=1):
-    all_news = []
-    base_url = f"https://www.tecmundo.com.br/novidades?page={page}"
-    content = fetch_content(base_url)
+def get_news_summary(content):
     selector = Selector(content)
-    for new in selector.css("div.tec--list__item"):
-        url = new.css("a.tec--card__title__link::attr(href)").get()
-        news = get_all_attributes(url)
-        all_news.append(news)
-    return all_news
+    summary = selector.css(".tec--article__body p").get()
+    summary_selector = Selector(summary)
+    summary_text = summary_selector.css("*::text").getall()
+    return ''.join(summary_text)
 
 
-print(scrape())
+def get_news_sources(content):
+    selector = Selector(content)
+    sources_list = selector.css(".tec--badge::text").getall()
+    if sources_list:
+        sources = map(lambda text: text.strip(), sources_list)
+        return list(sources)
+
+
+def get_news_categories(content):
+    selector = Selector(content)
+    categories_list = selector.xpath("//a[contains(@class, 'tec--badge') and contains(@class,
+    'tec--badge--primary')]/text()").getall()
+    if categories_list:
+        categories = map(lambda text: text.strip(), categories_list)
+        return list(categories)
+
+
+def get_news_data(url):
+    success = False
+    attempts = 1
+    while not success and attempts <= 3:
+        response = requests.get(url)
+        # Solução do Henrique Eyer pra verificar o retorno dos dados != None - https://github.com/tryber/sd-01-tech-news/tree/exemplo-tech-news-henriqueeyer
+        contentEncoding = response.headers['Content-Encoding']
+        success = contentEncoding == 'gzip'
+        attempts += 1
+
+        if response.status_code == 404:
+            attempts = 4
+        elif response.status_code == 429:
+            time.sleep(6)
+        if success:
+            return {
+                "url": url,
+                "title": get_news_title(response.text),
+                "timestamp": get_news_timestamp(response.text),
+                "writer": get_news_writer(response.text),
+                "shares_count": get_news_shares(response.text),
+                "comments_count": get_news_comments(response.text),
+                "summary": get_news_summary(response.text),
+                "sources": get_news_sources(response.text),
+                "categories": get_news_categories(response.text)
+            }
+
+
+def validate_data(data):
+    for element in data:
+        if data[element] is None:
+            return False
+        return True
+
+
+def scrape(pages=1):
+    page_url = "https://www.tecmundo.com.br/novidades"
+    current_page = 1
+    news_data = []
+    while current_page <= pages:
+        response = requests.get(page_url)
+        selector = Selector(text=response.text)
+        news_url = selector.css(".tec--card__title__link::attr(href)").getall()
+
+        for news in news_url:
+            data = get_news_data(news)
+            if data and validate_data(data):
+                news_data.append(data)
+
+        current_page += 1
+        page_url = f"https://www.tecmundo.com.br/novidades?page={current_page}"
+
+    db_client = MongoClient()
+    db = db_client.tech_news
+    col = db["news"]
+    for news in news_data:
+        col.find_one_and_update({"url": news["url"]}, {"$set": news}, upsert=True)
+    db_client.close()
+
+    print('Raspagem de notícias finalizada')
+    return news_data
+
+
+scrape()
