21a3b603d267169764493769f9f318ec0d25515f
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-08 13:43:10 -0300

    finalizando parte do CLI e item de importação

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,6 +1,47 @@
-def csv_importer():
-    raise NotImplementedError
+from pymongo import MongoClient
+import json
+import sys
 
 
-def json_importer():
-    raise NotImplementedError
+# Código baseado no projeto do Conrado
+# https://github.com/tryber/sd-01-tech-news/tree/conradomedeiros-sd-01-tech-news
+
+
+def file_exists(path):
+    if not path:
+        return False
+    True
+
+
+def validate_path_file(path, format):
+    array_of_path = path.split(".")
+    if array_of_path[1] == format:
+        return True
+    return False
+
+
+def json_importer(path_to_file="tech_news.json"):
+    if file_exists(path_to_file) is False:
+        return print(f"Arquivo {path_to_file} não encontrado")
+
+    if validate_path_file(path_to_file, "json") is False:
+        return print("Formato inválido")
+
+    with open(path_to_file) as file:
+        news = json.load(file)
+        for row in news:
+            if len(row) != 9:
+                return print(f"Erro na notícia {row[0]}")
+
+    client = MongoClient()
+    db = client.tech_news
+    try:
+        for notice in news:
+            db.news.find_one_and_update(
+                {"url": notice["url"]}, {"$set": notice}, upsert=True
+            )
+        client.close()
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+    print("Importação realizada com sucesso")
