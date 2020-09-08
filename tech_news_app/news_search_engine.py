8e6e8eecb1a196e8e5a8c00cd32138dfa80ffc3c
vrramosm <vf.ramos7@gmail.com>
2020-09-07 23:38:25 -0300

    Consertando requisitos do 6 ao 9

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,14 +1,74 @@
-def search_by_title():
-    raise NotImplementedError
+from pymongo import MongoClient
+
+# Código abaixo foi feito com a ajuda do conrado e coruja
+# https://github.com/tryber/sd-01-tech-news/tree/conradomedeiros-sd-01-tech-news
 
 
-def search_by_date():
-    raise NotImplementedError
+def get_data_from_database(search):
+    client = MongoClient()
+    db = client.tech_news
+    array_of_notices = []
+    try:
+        notices = db.news.find(search, {"_id": 0, "title": 1, "url": 1})
+        for notice in notices:
+            array_of_notices.append(notice)
+        client.close()
+        return array_of_notices
+    except client:
+        return print(client.errors)
 
 
-def search_by_source():
-    raise NotImplementedError
+def validate_title(title):
+    if len(title) == 0:
+        return False
 
 
-def search_by_category():
-    raise NotImplementedError
+def search_by_title(title):
+    if validate_title(title) is False:
+        return print("Dados inválidos")
+    received_data = get_data_from_database(
+        {"title": {"$regex": title, "$options": "i"}}
+        )
+    print("title", received_data)
+    return received_data
+
+
+def validate_date(date):
+    if len(date) == 0:
+        return []
+    splited_date = date.split("/")
+    return (
+        len(splited_date[0]) == 4
+        and len(splited_date[1]) == 2
+        and len(splited_date[2]) == 2
+    )
+
+
+def format_date(date):
+    replace_date = date.replace("/", "-")
+    return replace_date
+
+
+def search_by_date(date):
+    if validate_date(date) is False:
+        return print("Data inválida")
+    formated_date = format_date(date)
+    received_data = get_data_from_database({"timestamp": formated_date})
+    print("date", received_data)
+    return received_data
+
+
+def search_by_source(source):
+    received_data = get_data_from_database(
+        {"sources": {"$regex": f"^{source}", "$options": "i"}}
+        )
+    print("source", received_data)
+    return received_data
+
+
+def search_by_category(categories):
+    received_data = get_data_from_database(
+        {"categories": {"$regex": f"^{categories}", "$options": "i"}}
+        )
+    print("categories", received_data)
+    return received_data
