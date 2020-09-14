8493e9e3b58e12d9bf5b956749457f0ffb5f4127
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2020-09-14 16:47:10 -0300

    test: cria teste para validar o pytest evaluator

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,6 +1,10 @@
+import json
+
 def csv_importer():
-    raise NotImplementedError
+    return NotImplementedError
 
+def json_importer(file_path):
+    with open(file_path, 'r') as read_file:
+        news = json.load(read_file)
 
-def json_importer():
-    raise NotImplementedError
+    return news
